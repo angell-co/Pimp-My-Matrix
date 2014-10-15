eba1a9c10b767150de14fb882ff264715d1d404a
Josh Angell <josh@supercooldesign.co.uk>
2014-10-15 09:47:25 +0100

    check if we've already pimped buttons before adding them to prevent many copies of the buttons ui

--- pimpmymatrix/resources/js/pimpmymatrix.js
+++ pimpmymatrix/resources/js/pimpmymatrix.js
@@ -135,51 +135,58 @@ Craft.PimpMyMatrix = Garnish.Base.extend(
     {
       var buttonConfig = $.grep(this.buttonConfig, function(e){ return e.fieldHandle === matrixFieldHandle; });
 
-      // if we found one (and it has at least one group) execute our magic
+      // if we found one (and it has at least one group)
       if ( buttonConfig[0] !== undefined )
       {
 
-        // find and hide the original buttons
-        var $origButtons = $matrixField.find('> .buttons');
-        $origButtons.hide();
+        // find the original buttons
+        var $origButtons = $matrixField.find('> .buttons').first();
 
-        // make our own container
-        var $ourButtons = $('<div class="buttons pimped" />').insertAfter($origButtons),
-            $ourButtonsInner = $('<div class="btngroup" />').appendTo($ourButtons);
-
-        // loop each blockType / group pairing
-        var buttonObject = buttonConfig[0].config;
-        for (var key in buttonObject)
+        // from there, check if we've already pimped those buttons
+        if ( $origButtons.next('.buttons.pimped').length < 1 )
         {
 
-          // check if group exists, add if not
-          if ( $ourButtonsInner.find('[data-pimped-group="'+buttonObject[key]['group']+'"]').length === 0 )
+          // if we haven't already pimped them, hide the original ones and start the button pimping process
+          $origButtons.hide();
+
+          // make our own container
+          var $ourButtons = $('<div class="buttons pimped" />').insertAfter($origButtons),
+              $ourButtonsInner = $('<div class="btngroup" />').appendTo($ourButtons);
+
+          // loop each blockType / group pairing
+          var buttonObject = buttonConfig[0].config;
+          for (var key in buttonObject)
           {
-            $('<div class="btn  menubtn">'+buttonObject[key]['group']+'</div><div class="menu" data-pimped-group="'+buttonObject[key]['group']+'"><ul /></div>').appendTo($ourButtonsInner);
+
+            // check if group exists, add if not
+            if ( $ourButtonsInner.find('[data-pimped-group="'+buttonObject[key]['group']+'"]').length === 0 )
+            {
+              $('<div class="btn  menubtn">'+buttonObject[key]['group']+'</div><div class="menu" data-pimped-group="'+buttonObject[key]['group']+'"><ul /></div>').appendTo($ourButtonsInner);
+            }
+
+            // find sub group
+            $groupUl = $ourButtonsInner.find('[data-pimped-group="'+buttonObject[key]['group']+'"] ul');
+
+            // make link in new sub group
+            $('<li><a data-type="'+buttonObject[key]['blockType']['handle']+'">'+buttonObject[key]['blockType']['name']+'</a></li>').appendTo($groupUl);
+
           }
 
-          // find sub group
-          $groupUl = $ourButtonsInner.find('[data-pimped-group="'+buttonObject[key]['group']+'"] ul');
-
-          // make link in new sub group
-          $('<li><a data-type="'+buttonObject[key]['blockType']['handle']+'">'+buttonObject[key]['blockType']['name']+'</a></li>').appendTo($groupUl);
+          // make triggers MenuBtns
+          $ourButtonsInner.find('.menubtn').each(function()
+          {
+            new Garnish.MenuBtn($(this),
+            {
+              onOptionSelect: function(option)
+              {
+                // find our type and click the correct original btn!
+                var type = $(option).data('type');
+                $origButtons.find('[data-type="'+type+'"]').click();
+              }
+            });
+          });
 
         }
-
-        // make triggers MenuBtns
-        $ourButtonsInner.find('.menubtn').each(function()
-        {
-          new Garnish.MenuBtn($(this),
-          {
-            onOptionSelect: function(option)
-            {
-              // find our type and click the correct original btn!
-              var type = $(option).data('type');
-              $origButtons.find('[data-type="'+type+'"]').click();
-            }
-          });
-        });
-
       }
     }
 
