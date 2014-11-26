449841e49a450b72fa07c60672cd05eabce19187
Josh Angell <josh@supercooldesign.co.uk>
2014-11-26 09:56:51 +0000

    Get block models so we can have the block type id to match with our stored blocktype info

--- pimpmymatrix/controllers/PimpMyMatrixController.php
+++ pimpmymatrix/controllers/PimpMyMatrixController.php
@@ -23,8 +23,6 @@ class PimpMyMatrixController extends BaseController
     // get matrix field handle from POST
     $matrixFieldHandle = craft()->request->getPost('matrixFieldHandle');
 
-
-    // getBlockByHandle();
     $field = craft()->fields->getFieldByHandle($matrixFieldHandle);
     $blockTypes = craft()->matrix->getBlockTypesByFieldId($field->id);
 
@@ -41,6 +39,33 @@ class PimpMyMatrixController extends BaseController
       ));
     }
 
+  }
+
+  public function actionGetBlocks()
+  {
+    // Only ajax post requests
+    $this->requirePostRequest();
+    $this->requireAjaxRequest();
+
+    // get elemendIds from POST
+    $elemendIds = craft()->request->getPost('elemendIds');
+
+    $criteria = craft()->elements->getCriteria(ElementType::MatrixBlock);
+    $criteria->id = $elemendIds;
+    $blocks = $criteria->find();
+
+
+    // return blockTypeIdsByElementId or false
+    if ( $blocks ) {
+      $this->returnJson(array(
+        'success' => true,
+        'blocks' => $blocks
+      ));
+    } else {
+      $this->returnJson(array(
+        'success' => false
+      ));
+    }
   }
 
 }
--- pimpmymatrix/resources/js/pimpmymatrix.js
+++ pimpmymatrix/resources/js/pimpmymatrix.js
@@ -85,38 +85,67 @@ Craft.PimpMyMatrix = Garnish.Base.extend(
       var $matrixField = $(this),
           blockTypes = $matrixField.data('blockTypes');
 
-      // loop blocks if we have blockTypes
+      // if we have blockTypes
       if (typeof blockTypes !== "undefined") {
-        $matrixField.find('.matrixblock').each($.proxy(function()
+
+        // get elementIds of the blocks and return array of blockElementIds paired with blockTypeIds
+        var elementIds = $matrixField.find('.matrixblock').map(function() {
+          return $(this).data('id');
+        }).get();
+
+        Craft.postActionRequest('pimpMyMatrix/getBlocks', { elementIds : elementIds }, $.proxy(function(response, textStatus)
         {
-
-          // cache block
-          var $block = $(this);
-
-          // final check that we haven't already added one in case something has gone mental
-          if ( ! $block.hasClass('pimped') )
+          if (textStatus === 'success')
           {
-
-            // get the block type handle
-            var blockTypeHandle = $block.find('input[type="hidden"][name*="[type]"]').val();
-
-            // using the blockTypes, match the handle to the blockType object
-            var result = $.grep(blockTypes, function(e){ return e.handle === blockTypeHandle; });
-
-            // check we have something
-            if (result.length > 0)
+            if (response.success)
             {
 
-              // get the name and add it!
-              var blockName = result[0].name;
-              $block.addClass('pimped');
-              $block.prepend('<div class="pimpmymatrix-heading">'+blockName+'</div>')
+              // now we have an array of block models, store them
+              $matrixField.data('blocks', response.blocks);
+
+              // loop blocks
+              $matrixField.find('.matrixblock').each($.proxy(function()
+              {
+
+                // cache block
+                var $block = $(this);
+
+                // final check that we haven't already added one in case something has gone mental
+                if ( ! $block.hasClass('pimped') )
+                {
+
+                  // get the block element id
+                  var blockElementId = $block.data('id');
+
+                  // using the elementId, match up the block model
+                  var block = $.grep($matrixField.data('blocks'), function(e){ return e.id == blockElementId; });
+
+                  if (block.length > 0)
+                  {
+
+                    block = block[0];
+
+                    var blockType = $.grep(blockTypes, function(e){ return e.id == block.typeId; });
+
+                    if (blockType.length > 0)
+                    {
+
+                      // add the block name
+                      $block.addClass('pimped');
+                      $block.prepend('<div class="pimpmymatrix-heading">'+blockType[0].name+'</div>')
+
+                    }
+
+                  }
+
+                }
+
+              }), this);
 
             }
-
           }
-
         }), this);
+
       }
 
     }), this);
