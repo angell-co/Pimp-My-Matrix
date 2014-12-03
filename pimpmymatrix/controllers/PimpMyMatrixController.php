8385d59b48ba892f32cea21eb3078618307a8cab
Josh Angell <josh@supercooldesign.co.uk>
2014-12-03 10:33:07 +0000

    Removing block headings and assoc styles
    These are no longer needed as they were added in Craft 2.3

--- pimpmymatrix/controllers/PimpMyMatrixController.php
+++ pimpmymatrix/controllers/PimpMyMatrixController.php
@@ -13,34 +13,6 @@ namespace Craft;
 class PimpMyMatrixController extends BaseController
 {
 
-  public function actionGetBlockTypesFromField()
-  {
-    // Only ajax post requests
-    $this->requirePostRequest();
-    $this->requireAjaxRequest();
-
-
-    // get matrix field handle from POST
-    $matrixFieldHandle = craft()->request->getPost('matrixFieldHandle');
-
-    $field = craft()->fields->getFieldByHandle($matrixFieldHandle);
-    $blockTypes = craft()->matrix->getBlockTypesByFieldId($field->id);
-
-
-    // return blocktypes or false
-    if ( $blockTypes ) {
-      $this->returnJson(array(
-        'success' => true,
-        'blockTypes' => $blockTypes
-      ));
-    } else {
-      $this->returnJson(array(
-        'success' => false
-      ));
-    }
-
-  }
-
   public function actionGetBlocks()
   {
     // Only ajax post requests
