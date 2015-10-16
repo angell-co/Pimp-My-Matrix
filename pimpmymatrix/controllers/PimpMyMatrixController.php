5e54d592ca32e9b0b66db172fba0d554be6bc028
Josh Angell <josh@supercooldesign.co.uk>
2015-10-16 08:55:18 +0100

    Removing un-used function.

--- pimpmymatrix/controllers/PimpMyMatrixController.php
+++ pimpmymatrix/controllers/PimpMyMatrixController.php
@@ -13,31 +13,6 @@ namespace Craft;
 class PimpMyMatrixController extends BaseController
 {
 
-  public function actionGetBlocks()
-  {
-    // Only ajax post requests
-    $this->requirePostRequest();
-    $this->requireAjaxRequest();
 
-    // get elemendIds from POST
-    $elemendIds = craft()->request->getPost('elemendIds');
-
-    $criteria = craft()->elements->getCriteria(ElementType::MatrixBlock);
-    $criteria->id = $elemendIds;
-    $blocks = $criteria->find();
-
-
-    // return blockTypeIdsByElementId or false
-    if ( $blocks ) {
-      $this->returnJson(array(
-        'success' => true,
-        'blocks' => $blocks
-      ));
-    } else {
-      $this->returnJson(array(
-        'success' => false
-      ));
-    }
-  }
 
 }
