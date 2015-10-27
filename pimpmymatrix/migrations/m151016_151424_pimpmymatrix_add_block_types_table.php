51d138fc22134bd332f76848bf8935b607283bd9
Josh Angell <josh@supercooldesign.co.uk>
2015-10-27 10:45:52 +0000

    Break the settings migration out into its own one that happens after the table.

--- pimpmymatrix/migrations/m151016_151424_pimpmymatrix_add_block_types_table.php
+++ pimpmymatrix/migrations/m151016_151424_pimpmymatrix_add_block_types_table.php
@@ -30,59 +30,6 @@ class m151016_151424_pimpmymatrix_add_block_types_table extends BaseMigration
 		craft()->db->createCommand()->addForeignKey('pimpmymatrix_blocktypes', 'matrixBlockTypeId', 'matrixblocktypes', 'id', 'CASCADE', null);
 		craft()->db->createCommand()->addForeignKey('pimpmymatrix_blocktypes', 'fieldLayoutId', 'fieldlayouts', 'id', 'SET NULL', null);
 
-		// Migrate old settings across
-		$settings = craft()->db->createCommand()
-			->select('settings')
-			->from('plugins')
-			->where('class = :class', array(':class' => 'PimpMyMatrix'))
-			->queryScalar();
-
-		if ($settings)
-		{
-			$decodedSettings = JsonHelper::decode($settings);
-
-			if (isset($decodedSettings['buttonConfig']))
-			{
-
-				PimpMyMatrixPlugin::log('Migrating old settings', LogLevel::Info, true);
-				$buttonConfig = JsonHelper::decode($decodedSettings['buttonConfig']);
-
-				foreach ($buttonConfig as $row)
-				{
-					$field = craft()->fields->getFieldByHandle($row['fieldHandle']);
-
-					$blockTypes = craft()->matrix->getBlockTypesByFieldId($field->id);
-
-					foreach ($row['config'] as $config)
-					{
-
-						$selectedBlocktypeId = null;
-						foreach ($blockTypes as $blockType)
-						{
-							if ( $blockType->handle == $config['blockType']['handle'] )
-							{
-								$selectedBlocktypeId = $blockType->id;
-							}
-						}
-
-						$pimpedBlockType = new PimpMyMatrix_BlockTypeModel();
-						$pimpedBlockType->fieldId           = $field->id;
-						$pimpedBlockType->matrixBlockTypeId = $selectedBlocktypeId;
-						$pimpedBlockType->fieldLayoutId     = null;
-						$pimpedBlockType->groupName         = urldecode($config['group']);
-						$pimpedBlockType->context           = 'global';
-
-						$success = craft()->pimpMyMatrix_blockTypes->saveBlockType($pimpedBlockType);
-						if (!$success)
-						{
-							PimpMyMatrixPlugin::log("Config for the field {$row['fieldHandle']} could not be migrated", LogLevel::Info, true);
-						}
-					}
-				}
-				PimpMyMatrixPlugin::log('Done migrating old settings', LogLevel::Info, true);
-			}
-		}
-
 		return true;
 	}
 }
